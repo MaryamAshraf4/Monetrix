aa9a644908e70945c4b2521e6c74158a8df0ffa7
MaryamAshraf4 <maryamashraf386@gmail.com>
2025-07-07 16:17:54 +0300

    Add project files.

---
+++ Monetrix/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Monetrix.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
